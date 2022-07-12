4aecb32b9276770f9b05a23087200636b6638dc6
define16 <radix7318@gmail.com>
2022-07-13 02:50:35 +0900

    Add swagger and ajax example

---
+++ CommunityBoard/src/main/java/WebDev.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDev {
+    public static void main(String[] args) {
+        SpringApplication.run(WebDev.class, args);
+    }
+}
