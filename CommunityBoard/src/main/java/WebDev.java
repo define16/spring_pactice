fca5e2ab6aa5d18955b943cb1fed7034678e9b10
define16 <radix7318@gmail.com>
2022-07-15 02:16:42 +0900

    Fix component can not found error

--- CommunityBoard/src/main/java/WebDev.java
+++ CommunityBoard/src/main/java/WebDev.java
@@ -1,7 +1,15 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(
+        basePackages = {
+                "com.web",
+                "config",
+                "web",
+        }
+)
 public class WebDev {
     public static void main(String[] args) {
         SpringApplication.run(WebDev.class, args);
